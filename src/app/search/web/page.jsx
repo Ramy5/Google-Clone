5b83ae2fa9297e5c193bcbc35f2bbd8855b23885
RamySapry <ramyjounior464@gmail.com>
2023-05-11 23:26:04 +0300

    Fetch data from google developer

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
-const WebSearch = () => {
-  return <div>WebSearch</div>;
+const WebSearch = async ({ searchParams }) => {
+  const searchWord = searchParams.searchTerm;
+
+  const api = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT}&q=${searchWord}`
+  );
+
+  if (!api.ok) throw new Error("Something went wrong!");
+
+  const data = await api.json();
+
+  return (
+    <>
+      {data?.items.map((result) => (
+        <h2>{result.title}</h2>
+      ))}
+    </>
+  );
 };
 
 export default WebSearch;
